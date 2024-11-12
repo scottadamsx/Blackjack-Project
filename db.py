8f200a0cef61e441ee0fbad41b4b2e36e54bfaa3
Nathan Hayden <Nathan.Hayden@k202domain.csn.com>
2024-11-12 14:29:42 -0330

    consolidated our functions into seperate modules

--- db.py
+++ db.py
@@ -1,13 +1,11 @@
 def read_money():
     try:
-        with open("money.txt", "r") as file:
+        with open("data/money.txt", "r") as file:
             money = float(file.read())
-        return money
     except FileNotFoundError:
         print("Error: Money file not found. Starting with $100.")
+    return money
 
 def write_money(money):
-    with open("money.txt", "w") as file:
-            file.write(str(round(money, 2)))
-
-            
+    with open("data/money.txt", "w") as file:
+        file.write(str(round(money, 2)))
